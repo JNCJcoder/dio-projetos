235909c6f97afb288400eb2bb7baf9bded34d4be
JNCJcoder <jorgenettocjunior@gmail.com>
2023-09-26 12:34:26 -0300

    Desafio "Estruturas Condicionais com C#" concluido

---
+++ dio-csharp/Desafios/Verificação de Dados de Teste.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace VerificacaoDadosTeste {
+  class Program {
+    static void Main(string[] args) {
+      int idTeste;
+      if (int.TryParse(Console.ReadLine(), out idTeste)) {
+        string descricaoTeste = Console.ReadLine();
+
+        if (idTeste > 0 && descricaoTeste.Length <= 50) {
+          Console.WriteLine("ID de teste valido.\nDescricao valida.");
+        } else {
+          Console.WriteLine("ID de teste invalido ou descricao muito longa.");
+        }
+      } else {
+        Console.WriteLine("ID de teste invalido.");
+      }
+    }
+  }
+}
