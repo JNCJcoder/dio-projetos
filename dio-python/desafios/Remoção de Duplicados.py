4ea7d5bd31dd79451e6d3707caceb8254ce4578d
JNCJcoder <jorgenettocjunior@gmail.com>
2024-06-16 07:01:23 -0300

    Desafio "Remoção de Duplicados" concluido

---
+++ dio-python/desafios/Remoção de Duplicados.py
@@ -0,0 +1,9 @@
+# Recebe a lista do usuário
+entrada = input()
+# Converte a string de entrada em uma lista de números
+lista = [int(x.strip()) for x in entrada.split(',')]
+
+lista_unica = list(set(lista))
+
+# É Exibido a nova lista sem valores duplicados
+print(lista_unica)
