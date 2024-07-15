f5a9fa4a28dc5ae22d6e40c81d3d19dfa41b3856
JNCJcoder <jorgenettocjunior@gmail.com>
2024-07-14 22:42:36 -0300

    Desafio "Explorando o Mundo dos NFTs e Criptomoedas" concluido

---
+++ dio-python/desafios/Explorando o Mundo dos NFTs e Criptomoedas.py
@@ -0,0 +1,18 @@
+# Recebe a Entrada do usuário e armazena na variável "entrada"
+entrada = input()
+
+# Função responsável por receber um termo e retornar sua respectiva descrição.		
+def descrever_termo(termo):
+	if termo == "Tokenization":
+			return "Converte ativos em tokens digitais"
+
+	elif termo == "Blockchain":
+	    return "Habilita transações seguras e à prova de adulterações"
+	    
+	elif termo == "Smart Contracts":
+	    return "Programas autoexecutáveis que operam em blockchains"
+	    	    	
+	elif termo == "Decentralization":
+	    return "Remove autoridades centrais e devolve o controle aos usuários"
+	    
+print(descrever_termo(entrada))
