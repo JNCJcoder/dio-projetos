2d14f0d89d6e319d76628f819db500e6812dd177
JNCJcoder <jorgenettocjunior@gmail.com>
2024-09-03 19:34:05 -0300

    Desafio "Avaliando o Desempenho Anual dos Funcionários" concluido

---
+++ dio-csharp/Desafios/Avaliando o Desempenho Anual dos Funcionários.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        // Entrada das notas
+        string entrada = Console.ReadLine();
+        
+        // Separar os valores e convertê-los para inteiros
+        string[] notasString = entrada.Split(',');
+        int produtividade = int.Parse(notasString[0]);
+        int qualidade = int.Parse(notasString[1]);
+        int pontualidade = int.Parse(notasString[2]);
+        
+        double media = (produtividade + qualidade + pontualidade) / 3.0;
+
+        string elegivelParaBonus = media >= 7.0 ? "Sim" : "Nao";
+        
+        // Exibir os resultados
+        Console.WriteLine($"Media: {media}");
+        Console.WriteLine($"Elegivel para bonus: {elegivelParaBonus}");
+    }
+}
