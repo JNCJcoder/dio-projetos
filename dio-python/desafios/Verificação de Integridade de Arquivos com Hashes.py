7d1610606d94a4ac24a1b81ba3050f8f792565a8
JNCJcoder <jorgenettocjunior@gmail.com>
2024-12-11 14:39:23 -0300

    Desafio "Verificação de Integridade de Arquivos com Hashes" concluido.

---
+++ dio-python/desafios/Verificação de Integridade de Arquivos com Hashes.py
@@ -0,0 +1,16 @@
+def verificar_hashes(lista_hashes):
+  
+    for hash_comparacao in lista_hashes:
+        
+        hash_calculado, hash_esperado = hash_comparacao.split(",")
+
+        if hash_calculado.strip() == hash_esperado.strip():
+            print("Correto")
+        else:
+            print("Inválido")
+
+hashes_usuario = input()
+
+lista_hashes = input().split(";")
+
+verificar_hashes(lista_hashes)
